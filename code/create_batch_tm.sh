6df6b8c17de0e9185a8ce226437b6c29ca2a2f15
gergoe <gergoe@harras.be>
2024-02-22 16:32:47 +0100

    changes

--- code/create_batch_tm.sh
+++ code/create_batch_tm.sh
@@ -10,20 +10,62 @@
 ROOT="$(pwd)"
 mkdir -p $ROOT/repos
 mkdir -p $ROOT/offline
-common=$ROOT/repos/pisa_2025ft_translation_common
-omegat_bin_path=$ROOT/omegat
-config_dir_path=$ROOT/config
+common="${common_repo_path:-$ROOT/repos/pisa_2025ft_translation_common}"
+omegat_bin_path="${omegat_bin_path:-$ROOT/omegat/build/install/OmegaT}"
+config_dir_path="${omegat_config_dir_path:-$ROOT/config}"
+
 
 # @todo (maybe): change this value to "workflow" to overwrite the old file
 output_dir="tasks"
 
 # permanent params
-domain="https://git-codecommit.eu-central-1.amazonaws.com/v1/repos"
+domain="codecommit:/"
 
 # repo names to process should be added in data/repos.txt
 
 repo_name=pisa_2025${space}_translation_${locale}_${step}
 repo_url=$domain/$repo_name.git
+
+prune_tmx () {
+    local project_root=$1
+	local source_folder=$2
+	local destination_folder=$3
+	local batch_name=$4
+	
+	if ! [ -f $project_root/$destination_folder/$batch_name.tmx.before-prune ]; then
+		# add batch
+		cp -r $common/source/$batch_name $project_root/source
+		
+		# Use the batch tm as the working TM of the project.
+		mv $project_root/omegat/project_save.tmx $project_root/omegat/project_save.tmp
+		cp $project_root/$source_folder/$batch_name.tmx $project_root/omegat/project_save.tmx
+
+		# create batch master TM
+		java -jar $omegat_bin_path/OmegaT.jar $project_root --mode=console-translate --config-dir=$config_dir_path
+		
+		# Restore working TM
+		mv -f $project_root/omegat/project_save.tmp $project_root/omegat/project_save.tmx
+
+		# Make sure destination folder exists
+		mkdir -p $destination_folder
+
+		# rename destination file if it's the same as source (also a nice way to know what was already pruned)
+		if [ "$source_folder" = "$destination_folder" ] && [ -f $project_root/$destination_folder/$batch_name.tmx ]; then
+			mv $project_root/$destination_folder/$batch_name.tmx $project_root/$destination_folder/$batch_name.tmx.before-prune
+		fi
+
+		# Copy batch master TM to repo
+		local filename=(*-omegat.tmx)
+		cp $project_root/$filename $project_root/$destination_folder/$batch_name.tmx
+		# @todo (probably): replicate the path where the original batch TM is found under workflow, e.g. 
+		# ${repo_name}/${output_dir}/tm/auto/prev/$batch.tmx
+
+		# delete batch folder  before adding the next one
+		yes | rm -r $project_root/source/$batch
+	else
+		echo Batch $batch_name was already pruned earlier, skipping.
+	fi
+}
 
 for repo_name in $(cat $ROOT/data/repos.txt)
 do
@@ -75,31 +117,52 @@ do
     ##     batches+=( "$line" )
     ## done < <( xmlstarlet select -t -c "//mapping[contains(@local, 'source')]" $ROOT/repos/$repo_name/omegat.project | grep -Poh '(?<=local="source/)[^"]+' )
 
-    # @todo: point source folder in the project to common/source  ???? so that coping files isn't necessary?
-    batches="$(find $ROOT/offline/${repo_name}_OMT/workflow/ -type f -regextype egrep -regex '.*/(prev|next)/(01_COS_SCI-A_N|02_COS_SCI-B_N|03_COS_SCI-C_N|04_QQS_N|05_QQA_N|06_COS_LDW_N|07_COS_XYZ_N|08_CGA_SCI_N|11_COS_MAT-A_T|12_COS_MAT-B_T|13_COS_REA-A_T|14_COS_REA-B_T|15_COS_SCI-A_T|16_COS_SCI-B_T|17_CGA_SCI_T|18_CGA_MAT_T|19_CGA_REA_T|21_COSP_REA-A_T|22_COSP_REA-B_T|23_COSP_MAT-A_T|24_COSP_MAT-B_T|25_COSP_SCI-A_N|26_COSP_SCI-A_T).tmx' -exec basename {} \; | cut -d'.' -f1)"
-    for batch in $batches; 
+    # Prune /tm/auto/
+	batch_tms="$(find $ROOT/offline/${repo_name}_OMT/tm/auto/ -type f -regextype egrep -regex '.*/(prev|next)/([0-9]{2}_[-_a-zA-Z]+_[NT]).tmx')"
+    for tm_filename in $batch_tms; 
     do
-        # add batch
-        cp -r $common/source/$batch $ROOT/offline/${repo_name}_OMT/source
-
-        # create batch master TM
-        java -jar $omegat_bin_path/build/install/OmegaT/OmegaT.jar $ROOT/offline/${repo_name}_OMT --mode=console-translate --config-dir=$config_dir_path
-    
-        # copy batch master TM to repo
-        mkdir -p $ROOT/repos/${repo_name}/${output_dir}
-        cp $ROOT/offline/${repo_name}_OMT/${repo_name}_OMT-omegat.tmx $ROOT/repos/${repo_name}/${output_dir}/$batch.tmx
-        # @todo (probably): replicate the path where the original batch TM is found under workflow, e.g. 
-        # ${repo_name}/${output_dir}/tm/auto/prev/$batch.tmx
-    
-        # delete batch folder  before adding the next one
-        yes | rm -r $ROOT/offline/${repo_name}_OMT/source/$batch
-
+		src_dir=$(dirname $tm_filename)
+		tm_dir=$(realpath --relative-to="${ROOT}/offline/${repo_name}_OMT" $src_dir)
+		batch_name=$(basename $tm_filename | cut -d'.' -f1)
+		echo Processing $batch_name in $tm_dir...
+		
+		# We are pruning the TM's received from the previous or next workflow steps. Batch TMs (found in /tm/auto/prev|next/) in the current locale
+		# should only have segments from the batch they were produced in.
+		prune_tmx "${ROOT}/offline/${repo_name}_OMT" $tm_dir $tm_dir $batch_name
+		
+		# The result file is $tm_filename (because we are overwriting the offline copy above), now we can copy that into the real repo.
+		# We're also copying ${batch_name}.tmx.before-prune if present
+		cp $tm_filename $ROOT/repos/$repo_name/$tm_dir
+		cp "${tm_filename}.before-prune" $ROOT/repos/$repo_name/$tm_dir
     done
-    
-    # commit changes (new files)
+	# Commit
     cd $ROOT/repos/$repo_name
-    git add ${output_dir}/*.tmx
-    git commit -m "Added batch TMs for finalized batches/tasks"
+    git add tm/auto
+    git commit -m "Pruned TMs in tm/auto"
+
+    # Prune /workflow/tm/auto/
+    workflow_tms="$(find $ROOT/offline/${repo_name}_OMT/workflow/tm/auto/ -type f -regextype egrep -regex '.*/(prev|next)/([0-9]{2}_[-_a-zA-Z]+_[NT]).tmx')"
+    for tm_filename in $workflow_tms; 
+    do
+		src_dir=$(dirname $tm_filename)
+		tm_dir=$(realpath --relative-to="${ROOT}/offline/${repo_name}_OMT" $src_dir)
+		batch_name=$(basename $tm_filename | cut -d'.' -f1)
+		echo Processing $batch_name in $tm_dir...
+		
+		# We are pruning the Workflow TMs - to be on the safe side?
+		prune_tmx "${ROOT}/offline/${repo_name}_OMT" $tm_dir $tm_dir $batch_name
+		
+		# The result file is $tm_filename (because we are overwriting the offline copy above), now we can copy that into the real repo.
+		# We're also copying ${batch_name}.tmx.before-prune if present
+		cp $tm_filename $ROOT/repos/$repo_name/$tm_dir
+		cp "${tm_filename}.before-prune" $ROOT/repos/$repo_name/$tm_dir
+    done
+	# Commit
+    cd $ROOT/repos/$repo_name
+    git add workflow/tm/auto
+    git commit -m "Pruned TMs in workflow/tm/auto"
+    
+    # Push changes (new files)
     git push
     
     # clean up the mess
@@ -107,5 +170,4 @@ do
     yes | rm -r $ROOT/repos/${repo_name}/
 
 done
-
 # after that, the PISA workflow service can fetch those batch TMs and add them to the next step
